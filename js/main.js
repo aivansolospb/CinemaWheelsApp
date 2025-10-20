a419ce200100780464f8b209eda590134d6a07d9
aivansolospb <aivansolo.spb@gmail.com>
2025-10-20 20:41:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
 // --- Глобальные переменные ---
 const tg = window.Telegram.WebApp;
-const APP_VERSION = 'Silent Validation';
+const APP_VERSION = 'Profile Fix';
 let _ACCESS_METHOD = 'Неизвестно';
 let _TG_ID = '';
 let _TG_USERNAME = '';

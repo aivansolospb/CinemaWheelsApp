6aeaa5b81c1c40d26ed5995cb629dbe467547125
aivansolospb <aivansolo.spb@gmail.com>
2025-10-20 21:00:08 +0300

    Profile Layout Fix

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
 // --- Глобальные переменные ---
 const tg = window.Telegram.WebApp;
-const APP_VERSION = 'Profile Fix v2';
+const APP_VERSION = 'Profile Layout Fix';
 let _ACCESS_METHOD = 'Неизвестно';
 let _TG_ID = '';
 let _TG_USERNAME = '';
